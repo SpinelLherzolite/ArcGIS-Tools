528f53c4c372d83e0de7a244a164d2da4f2409b9
Marty <62304623+SpinelLherzolite@users.noreply.github.com>
2020-10-05 17:38:56 -0600

    Update Lidar clipping.py

--- Lidar clipping.py
+++ Lidar clipping.py
@@ -16,19 +16,24 @@ env.workspace = r'C:\Users\mpalkovic\Documents\ArcGIS\Projects\WeldCoMaps\WeldCo
 env.overwriteOutput = False
 env.addOutputsToMap = False
 
+#definition query
+#---------------
 aprx = arcpy.mp.ArcGISProject('CURRENT')
 m = aprx.listMaps('WeldCo')[0]
 for l in m.listLayers():
     if l.name == 'COUNTIES_DOLA_2016':
         l.definitionQuery = """"COUNTY" = 'WELD'"""
 
+#define variables
+#----------------
 inLidar = r'N:\LIBRARY\Data\GIS data library\LiDAR\Lincoln, Elbert, Arapahoe, Adams, Denver, Morgan, Weld counties composite\Blocks_1_4\total_mosaic.img'
 inQuads = 'Quads24k_USDA_python' #SelectLayerByLocation wont accept a file path for an input, must be local to the mxd/aprx project
 inCounty = 'COUNTIES_DOLA_2016'
 fields = ['quad_name', 'SHAPE@']
 arcpy.SelectLayerByLocation_management (inQuads, 'WITHIN', inCounty)
 
-#----------------------------------------
+#iterate through each row in a feature class using a search cursor
+#-----------------------------------------------------------------
 with arcpy.da.SearchCursor (inQuads, fields) as cursor:
     for row in cursor:
         try:
@@ -44,28 +49,20 @@ with arcpy.da.SearchCursor (inQuads, fields) as cursor:
             pass
 
 #delete the buffered polygons
+#----------------------------
 dList = [fc for fc in arcpy.ListFeatureClasses() if fc.endswith('bf')]
 #print (dList)
 for fc in dList:
     arcpy.Delete_management(fc)
 
 #Build pyramids for the rasters
+#-------------------------------
 pList = arcpy.ListRasters()
 print (pList)
 
-#Build pyramids for rasters in env.workspace
 for raster in pList:
     try:
         arcpy.BuildPyramids_management(raster)
     except:
         print (arcpy.GetMessages())
         pass   
-
-
-#with arcpy.da.SearchCursor (inQuads, fields) as cursor:
-#    for row in cursor:
-#        outFC = os.path.join(env.workspace, (row[0][:13]))
-#        outClip = arcpy.Clip_management(inLidar, '#', outFC, row[1], '#', 'ClippingGeometry', 'NO_MAINTAIN_EXTENT')
-#        arcpy.HillShade_3d(outClip, (row[0][:10]) + '_hs')
-#        arcpy.Slope_3d(outClip, (row[0][:10]) + '_sl')
-#        arcpy.Aspect_3d(outClip, (row[0][:10]) + '_as')
